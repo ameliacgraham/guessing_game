58c1a3f2b3d059b7ac1e05b9e73f0e8d29e9cc86
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-21 12:38:51 -0800

    Getting the Player's Name

--- game.py
+++ game.py
@@ -4,4 +4,4 @@
 
 print 'Hello, welcome to the guessing game!'
 player_name = raw_input('What is your name? ')
-print "Hi %s" % player_name
+print "Hi %s!" % player_name # greet the player by their name
