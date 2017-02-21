36f52ab0222a85b02e159f8573126e426541052f
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-21 12:56:18 -0800

    Random number is between 1 and 100

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 from random import randint
 
-rand_num = randint(0, 101) 
+rand_num = randint(1, 101) 
 
 print 'Hello, welcome to the guessing game!'
 player_name = raw_input('What is your name? ')
@@ -16,7 +16,6 @@ while True:
     elif guess > rand_num:
         print "Your guess is too high, try again."
         guesses += 1
-
     else:
         print "You guessed the right number! You found my number in %i tries" % (guesses)
         break
