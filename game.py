db61dee27ee5643cc74f31459da770ea9bba4fc7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-21 15:19:58 -0800

    Exception handling for value error

--- game.py
+++ game.py
@@ -9,17 +9,24 @@ print "Hi %s!" % player_name # greet the player by their name
 guesses = 0
 guess = 101
 while True:
-    guess = int(raw_input('Guess a number? '))
-    if guess >= 1 and guess <= 100:
-        if guess < rand_num:
-            print "Your guess is too low, try again."
-            guesses += 1
-        elif guess > rand_num:
-            print "Your guess is too high, try again."
-            guesses += 1
-        else:
-            print "You guessed the right number! You found my number in %i tries" % (guesses)
+    while True:
+        try:
+            guess = int(raw_input('Guess a number? '))
             break
-    else:
-        print "Oops, your guess was not between 1 and 100"
-        continue  
+        except ValueError:
+            print "Opps! That was not a number. Try again"
+            continue
+
+        if guess >= 1 and guess <= 100:
+            if guess < rand_num:
+                print "Your guess is too low, try again."
+                guesses += 1
+            elif guess > rand_num:
+                print "Your guess is too high, try again."
+                guesses += 1
+            else:
+                print "You guessed the right number! You found my number in %i tries" % (guesses)
+                break
+        else:
+            print "Oops, your guess was not between 1 and 100"
+            continue  
