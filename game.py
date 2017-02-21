77e15f43a227988b63e0f146db87302c8ebf6d17
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-21 12:35:52 -0800

    Greeting the Player

---
+++ game.py
@@ -0,0 +1,7 @@
+# from random import randint
+
+# rand_num = randint(0, 101) 
+
+print 'Hello, welcome to the guessing game!'
+player_name = raw_input('What is your name? ')
+print "Hi %s" % player_name
