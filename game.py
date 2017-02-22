2d40d51ad2348fad2f558c34cd938db2a0a2f96f
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-21 17:05:09 -0800

    low score implemented with integer instead of list

--- game.py
+++ game.py
@@ -1,15 +1,15 @@
 from random import randint
+from sys import maxint
 
 player_name = raw_input('What is your name? ')
 print "Hi %s!" % player_name # greet the player by their name
-scores = []
 
+low_score = maxint
 while True:
     rand_num = randint(1, 100)
-
     print 'Hello, welcome to the guessing game!'
 
-    num_of_guesses = 0
+    num_of_guesses = 1
     guess = 101
     print rand_num 
     while True:
@@ -23,7 +23,11 @@ while True:
                     print "Your guess is too high, try again."
                     num_of_guesses += 1
                 else:
-                    scores.append(num_of_guesses)
+                    print "number of guesses: %i" % num_of_guesses
+                    print "low score is originally: %i" % low_score
+                    if num_of_guesses < low_score:
+                        low_score = num_of_guesses
+                        print "low score is now: %i" % low_score 
                     print "You guessed the right number! You found my number in %i tries" % (num_of_guesses)
                     print "Would you like to play again? y/n"
                     answer = raw_input().lower()
@@ -37,8 +41,6 @@ while True:
                             print "Not a valid answer!"
                             print "Please enter y or n"
                             answer = raw_input("> ")
-
-
             else:
                 print "Oops, your guess was not between 1 and 100"
                 continue
@@ -46,6 +48,5 @@ while True:
             print "Opps! That was not a number. Try again"
             continue
     if answer == "n":
-        scores.sort()
-        print "Your best score was %i" % (scores[0])
+        print "Your best score was %i" % (low_score)
         break        
